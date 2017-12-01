a09bed002d44e30c02845d342149f287351f0350
awu12 <tempatwisatabaru@gmail.com>
2017-12-01 18:47:01 +0700

    Update datarandom.js

--- datarandom.js
+++ datarandom.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/12/managing-virtual-data-room-in-early.html"}
 if(x=="2"){xxx="/2017/12/when-to-use-virtual-data-room.html"}
 if(x=="3"){xxx="/2017/12/5-factors-to-compare-virtual-data-room.html"}
 if(x=="4"){xxx="/2017/12/sharefile-virtual-data-room.html"}
-if(x=="5"){xxx="/2017/12/best-virtual-data-room-providers.html"}a_to_ve[i].href="http://www.datalink.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/12/best-virtual-data-room-providers.html"}a_to_ve[i].href="http://www.datalinks.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
